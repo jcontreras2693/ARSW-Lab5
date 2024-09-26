4b84040faab95270343363b91dec712472e29da7
unknown <juan.cbecerra@labinfo.is.escuelaing.edu.co>
2024-09-26 10:00:11 -0500

    Parte I

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -1,40 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.blueprints.controllers;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import edu.eci.arsw.blueprints.persistence.*;
+import edu.eci.arsw.blueprints.model.Blueprint;
 import edu.eci.arsw.blueprints.services.BlueprintsServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Set;
 
-/**
- *
- * @author hcadavid
- */
 @RestController
 @RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
+
     private final BlueprintsServices blueprintsServices;
+
     @Autowired
-    public BlueprintAPIController(BlueprintsServices blueprintsServices){
-        this.blueprintsServices =  blueprintsServices;
+    public BlueprintAPIController(BlueprintsServices blueprintsServices) {
+        this.blueprintsServices = blueprintsServices;
     }
+
+    // Maneja el GET para obtener los planos de un autor específico
+    @RequestMapping(value = "/{author}", method = RequestMethod.GET)
+    public ResponseEntity<?> obtenerPlanosPorAutor(@PathVariable("author") String autor) {
+        try {
+            Set<?> bp = blueprintsServices.getBlueprintsByAuthor(autor);
+            if (bp.isEmpty()) {
+                return new ResponseEntity<>("AUTHOR NOT FOUND", HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(bp, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("INTERNAL SERVER ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    // Maneja el GET para obtener los planos de un autor por nombre de autor y plano
+    @RequestMapping(value = "/{author}/{blueprint}", method = RequestMethod.GET)
+    public ResponseEntity<?> obtenerPlanosPorAutorPlano(@PathVariable("author") String autor, @PathVariable("blueprint") String blueprint) {
+        try {
+            Blueprint bp = blueprintsServices.getBlueprint(autor, blueprint);
+            if (bp.equals(null)) {
+                return new ResponseEntity<>("BLUEPRINT NOT FOUND", HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(bp, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("INTERNAL SERVER ERROR", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> manejadorGetRecursoXX() {
-        //obtener datos que se enviarán a través del API
+        // Obtener todos los datos que se enviarán a través del API
         return new ResponseEntity<>(blueprintsServices.getAllBlueprints(), HttpStatus.ACCEPTED);
     }
 }
